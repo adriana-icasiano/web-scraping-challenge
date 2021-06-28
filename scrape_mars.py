a57b0898e77eb7e3924901c163c52200ec1fcb03
adriana-icasiano <81338603+adriana-icasiano@users.noreply.github.com>
2021-06-28 16:15:16 -0400

    copied and pasted codes to scrape file for 3 out of 4 website

---
+++ scrape_mars.py
@@ -0,0 +1,74 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+import requests
+import pandas as pd
+
+
+def scrape():
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Visit visitcostarica.herokuapp.com
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Get the latest news
+    results = soup.find_all('div', class_='list_text')
+
+    news = {"headline":[],"paragraph":[]}
+        
+    for result in results:
+    
+        headline = result.find('div', class_='content_title').text
+        paragraph = result.find('div', class_='article_teaser_body').text
+        print(headline)
+        print(paragraph)
+        news['headline'].append(headline)
+        news['paragraph'].append(paragraph)
+    
+    # Store data in variables
+    news_title = news['headline'][0]
+    news_p = news['paragraph'][0]
+
+    #Get the featured image
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Store image in variables
+    image_url = "https://spaceimages-mars.com/"+soup.find('img', class_='headerimage fade-in')['src']
+
+
+    # Get Mars facts
+    url = 'https://galaxyfacts-mars.com'
+
+    # Read html tables
+    tables = pd.read_html(url)
+
+    # Create dataframe from table
+    df = tables[0]
+    df.head()
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = new_header
+
+    # Convert dataframe to html table, and store as variable
+    html_table = df.to_html()
+
+    # Quit the browser after scraping
+    browser.quit()
+
+    # Return results
+    return costa_data
